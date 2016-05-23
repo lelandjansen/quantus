2f06c2406a59fa42f5f6e272d44c2cb917a24bb7
Leland Jansen <lelandjansen@users.noreply.github.com>
2016-05-23 11:37:37 -0400

    Base Xcode application

---
+++ software/Mac/quantus/quantus/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  quantus
+//
+//  Created by Leland Jansen on 2016-05-23.
+//  Copyright © 2016 Leland Jansen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ software/Mac/quantus/quantus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  quantus
+//
+//  Created by Leland Jansen on 2016-05-23.
+//  Copyright © 2016 Leland Jansen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
